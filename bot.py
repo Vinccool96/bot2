c43992ffc406f35c227411e1d3568c510e9e081f
VincentGirard7 <vincent.girard.7@ulaval.ca>
2018-11-21 09:03:12 -0500

    renamed file

--- bot.py
+++ bot.py
@@ -17,17 +17,17 @@ def sub_exists(subre):
     return exists
 
 
-dict = '0123456789abcdefghijklmnopqrstuvwxyz-_'
+characs = '0123456789abcdefghijklmnopqrstuvwxyz-_'
 
 reddit = praw.Reddit(client_id=conf.get('Bot2', 'client_id'), client_secret=conf.get('Bot2', 'client_secret'),
                      user_agent=conf.get('Bot2', 'user_agent'), password=conf.get('Bot2', 'password'),
                      username=conf.get('Bot2', 'username'))
 
-names = combinations.comb_ordr_rep__of_len_in_range_str(dict, 3, 4)
+names = combinations.comb_ordr_rep__of_len_in_range_str(characs, 3, 4)
 
 print('combinations done', len(names))
 
-fil = open('subreddits.txt', 'a')
+fil = open('subreddits3chars.txt', 'a')
 
 for sub in names:
     try:
