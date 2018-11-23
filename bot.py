7df1b1850430bef94e6e974fa0cbebecee1856aa
VincentGirard7 <vincent.girard.7@ulaval.ca>
2018-11-23 00:04:58 -0500

    stopped for now

--- bot.py
+++ bot.py
@@ -50,37 +50,17 @@ list_subs = []
 for infos in list_infos:
     list_subs += [infos[3]]
 
+list_infos = None
 list_subs_sorted = sorted(list_subs, key=str.upper)
+list_subs = None
 
 for sub_sorted in list_subs_sorted:
-    fil_subredds.write(sub_sorted + '\n')
+    fil_subredds.write(sub_sorted + ('\n' if sub_sorted != list_subs_sorted[-1] else ''))
+
+fil_subredds.close()
 
 time_subs_sorted = datetime.datetime.utcnow()
 print('time subs are sorted: ' + str(time_subs_sorted))
-
-fil_subredds_at_large = open('subredds_at_large.txt', 'w', newline='\n')
-
-fil_subredds_public = open('subredds_public.txt', 'w', newline='\n')
-
-fil_subredds_is_it_nsfw = open('subredds_is_it_nsfw.txt', 'w', newline='\n')
-
-fil_subredds_sfw = open('subredds_sfw.txt', 'w', newline='\n')
-
-fil_subredds_nsfw = open('subredds_nsfw.txt', 'w', newline='\n')
-
-fil_subredds_gold_restricted = open('subredds_gold_restricted.txt', 'w', newline='\n')
-
-list_subredds_at_large = []
-
-list_subredds_public = []
-
-list_subredds_is_it_nsfw = []
-
-list_subredds_sfw = []
-
-list_subredds_nsfw = []
-
-list_subredds_gold_restricted = []
 
 time_started_to_get_infos = datetime.datetime.utcnow()
 print('time started to get infos: ' + str(time_started_to_get_infos))
@@ -88,33 +68,23 @@ print('time started to get infos: ' + str(time_started_to_get_infos))
 for sub in list_subs_sorted:
     res = client.get(f'https://www.reddit.com/r/{sub}/about/.json')
     rep = json.loads(res.text)
-    if rep.keys().__contains__('error'):
+    if rep.keys().__contains__('error') or rep.keys().__contains__('reason'):
         continue
     elif rep['kind'] == 't5':
-        list_subredds_at_large.append(sub)
+        with open('subredds_at_large.txt', 'a', newline='\n') as fil_subredds_at_large:
+            fil_subredds_at_large.write(sub+'\n')
         if rep['data']['subreddit_type'] == 'public':
-            list_subredds_public.append(sub)
+            with open('subredds_public.txt', 'a', newline='\n') as fil_subredds_public:
+                fil_subredds_public.write(sub+'\n')
             isOver18 = rep['data']['over18']
-            list_subredds_is_it_nsfw.append(sub + ' is NSFW: ' + str(isOver18))
+            with open('subredds_is_it_nsfw.txt', 'a', newline='\n') as fil_subredds_is_it_nsfw:
+                fil_subredds_is_it_nsfw.write(sub + ' is NSFW: ' + str(isOver18)+'\n')
             print(sub + ' is NSFW: ' + str(isOver18))
-            (list_subredds_nsfw if isOver18 else list_subredds_sfw).append(sub)
-        if rep['data']['subreddit_type'] == 'gold_restricted':
-            list_subredds_gold_restricted.append(sub)
-
-time_infos_received = datetime.datetime.utcnow()
-print('time info is received: ' + str(time_infos_received))
-
-zips = [(fil_subredds_at_large, list_subredds_at_large), (fil_subredds_public, list_subredds_public),
-        (fil_subredds_is_it_nsfw, list_subredds_is_it_nsfw), (fil_subredds_sfw, list_subredds_sfw),
-        (fil_subredds_nsfw, list_subredds_nsfw), (fil_subredds_gold_restricted, list_subredds_gold_restricted)]
-
-time_started_writing_infos = datetime.datetime.utcnow()
-print('time started writing infos: ' + str(time_started_writing_infos))
-
-for z in zips:
-    f, l_subs = z[0], z[1]
-    for s in l_subs:
-        f.write(s + '\n')
+            with open(f"subredds_{'nsfw' if isOver18 else 'sfw'}.txt", 'a', newline='\n') as fil_subredds_xsfw:
+                fil_subredds_xsfw.write(sub+'\n')
+        elif rep['data']['subreddit_type'] == 'gold_restricted':
+            with open('subredds_gold_restricted.txt', 'a', newline='\n') as fil_subredds_gold_restricted:
+                fil_subredds_gold_restricted.write(sub+'\n')
 
 time_ended = datetime.datetime.utcnow()
 print('time ended: ' + str(time_ended))
@@ -124,6 +94,4 @@ with open('timestamps.txt', 'w') as fil_timestamps:
     fil_timestamps.write('time to get list of subs: ' + str(time_started_to_get_list_of_subs) + '\n')
     fil_timestamps.write('time subs are sorted: ' + str(time_subs_sorted) + '\n')
     fil_timestamps.write('time started to get infos: ' + str(time_started_to_get_infos) + '\n')
-    fil_timestamps.write('time info is received: ' + str(time_infos_received) + '\n')
-    fil_timestamps.write('time started writing infos: ' + str(time_started_writing_infos) + '\n')
     fil_timestamps.write('time ended: ' + str(time_ended) + '\n')
